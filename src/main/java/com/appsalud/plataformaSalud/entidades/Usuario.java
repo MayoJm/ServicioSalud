8e6808c20caa8a2853d3ec5da3c7f933b1873649
Efimero2004 <santiago.barra@est.fi.uncoma.edu.ar>
2024-01-31 20:10:11 -0300

    se agrega entidad usuario, usuarioProfesional y enum rol

--- src/main/java/com/appsalud/plataformaSalud/entidades/Usuario.java
+++ src/main/java/com/appsalud/plataformaSalud/entidades/Usuario.java
@@ -1,13 +1,39 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.appsalud.plataformaSalud.entidades;
 
-/**
- *
- * @author santi
- */
+import com.appsalud.plataformaSalud.enumeraciones.Rol;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+
+@Getter
+@Setter
+@Entity
 public class Usuario {
-    
+
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    private String id;
+
+    private String nombre;
+    private String email;
+    private String password;
+    private String dni;
+    private String direccion;
+    private String telefono;
+    private Boolean estado;
+
+    //@OneToOne
+    //private Imagen imagen;
+    //@OneToOne
+    //private Calendario calendario;
+    @Enumerated(EnumType.STRING)
+    private Rol rol;
+
 }
---
+++ src/main/java/com/appsalud/plataformaSalud/enumeraciones/Rol.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package com.appsalud.plataformaSalud.enumeraciones;
+
+/**
+ *
+ * @author santi
+ */
+public enum Rol {
+    ADMIN,
+    PROFESIONAL,
+    PACIENTE;
+}
