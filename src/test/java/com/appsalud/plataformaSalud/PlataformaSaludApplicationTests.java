3d3ded94ff0ac2eae175e2c8f2ce17cd2bc09eb3
juandiazmayo <juandiazmaio@gmail.com>
2024-01-31 01:28:10 -0300

    carga del proyecto en github

---
+++ src/test/java/com/appsalud/plataformaSalud/PlataformaSaludApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appsalud.plataformaSalud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlataformaSaludApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
