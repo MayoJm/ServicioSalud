b1acea9e3a69121b37ba5a35284226795fa7a424
ayrtonjofre <ajofre@nextbyn.com>
2024-02-01 01:49:33 -0300

    Modificaciones en constructores, con uso de anotaciones LomBok

--- src/main/java/com/appsalud/plataformaSalud/entidades/Usuario.java
+++ src/main/java/com/appsalud/plataformaSalud/entidades/Usuario.java
@@ -7,13 +7,18 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.GenericGenerator;
 
+@Entity
 @Getter
 @Setter
-@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class Usuario {
 
     @Id
--- src/main/java/com/appsalud/plataformaSalud/entidades/UsuarioPaciente.java
+++ src/main/java/com/appsalud/plataformaSalud/entidades/UsuarioPaciente.java
@@ -4,20 +4,20 @@ import com.appsalud.plataformaSalud.enumeraciones.ObraSocial;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class UsuarioPaciente extends Usuario{
     @Enumerated(EnumType.STRING)
     private ObraSocial obraSocial;
     
 //    private EstudiosClinicos estudiosClinicos; //A DEBATIR
-
-    public UsuarioPaciente() {
-    }
-    
-    
 }
