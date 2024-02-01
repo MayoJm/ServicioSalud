983d6c0d461b339e64b25e9bd1c75925d0fcbf87
juandiazmayo <juandiazmaio@gmail.com>
2024-02-01 17:59:31 -0300

    SeguridadWeb basica, vista LandingPage con login y register de usuario

--- src/main/java/com/appsalud/plataformaSalud/controladores/UsuarioControlador.java
+++ src/main/java/com/appsalud/plataformaSalud/controladores/UsuarioControlador.java
@@ -1,46 +1,20 @@
 package com.appsalud.plataformaSalud.controladores;
 
-import com.appsalud.plataformaSalud.excepciones.MiException;
-import com.appsalud.plataformaSalud.servicios.UsuarioServicio;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/user")
 public class UsuarioControlador {
-    
-    @Autowired
-    UsuarioServicio usuarioServicio;
-    
+
+   
+
     @GetMapping("/modificar/{email}")
-    public String modificarUsuario(@PathVariable String email){
+    public String modificarUsuario(@PathVariable String email) {
         return ""; //vista de modificar_usuario.html (pendiente)
     }
-    
-    @GetMapping("/registrar")
-    public String registrar() {
-        return "usuario_form.html"; //pendiente
-    }
-    
-     @PostMapping("/registro")
-    public String registro(@RequestParam String nombre, String apellido, String email, String password, String password2, ModelMap modelo) {
 
-        try {
-            usuarioServicio.crearUsuario(nombre, apellido,  email, password, password2);
-
-            modelo.put("exito", "El Usuario fue registrado correctamente!");
-        } catch (MiException ex) {
-
-            modelo.put("error", ex.getMessage());
-            return "usuario_form.html";
-        }
-
-        return "inicio.html"; //html pendiente
-    }
+   
 }
--- src/main/java/com/appsalud/plataformaSalud/entidades/Usuario.java
+++ src/main/java/com/appsalud/plataformaSalud/entidades/Usuario.java
@@ -6,7 +6,6 @@ import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
