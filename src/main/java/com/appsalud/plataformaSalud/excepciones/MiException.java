ea02f82e49b280602e61e525b04cd50fc5ba999f
Efimero2004 <santiago.barra@est.fi.uncoma.edu.ar>
2024-01-31 21:45:34 -0300

    se agrega package excepciones con la clase MiException

---
+++ src/main/java/com/appsalud/plataformaSalud/excepciones/MiException.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.appsalud.plataformaSalud.excepciones;
+
+/**
+ *
+ * @author santi
+ */
+public class MiException extends Exception{
+    public MiException(String msg){
+        super(msg);
+    }
+    
+}
