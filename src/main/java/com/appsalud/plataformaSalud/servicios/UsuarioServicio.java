2d37ae94534ea2c30b2f8ac7222d62f5af262e29
Efimero2004 <santiago.barra@est.fi.uncoma.edu.ar>
2024-02-05 20:15:38 -0300

    subiendo modificacion en UsuarioServicio y UsuarioRepositorio

--- src/main/java/com/appsalud/plataformaSalud/servicios/UsuarioServicio.java
+++ src/main/java/com/appsalud/plataformaSalud/servicios/UsuarioServicio.java
@@ -1,6 +1,10 @@
 package com.appsalud.plataformaSalud.servicios;
 
 import com.appsalud.plataformaSalud.entidades.Usuario;
+import com.appsalud.plataformaSalud.entidades.UsuarioPaciente;
+import com.appsalud.plataformaSalud.entidades.UsuarioProfesional;
+import com.appsalud.plataformaSalud.enumeraciones.Especialidad;
+import com.appsalud.plataformaSalud.enumeraciones.ObraSocial;
 import com.appsalud.plataformaSalud.enumeraciones.Rol;
 import com.appsalud.plataformaSalud.excepciones.MiException;
 import com.appsalud.plataformaSalud.repositorios.UsuarioRepositorio;
@@ -20,60 +24,149 @@ public class UsuarioServicio {
     private UsuarioRepositorio usuarioRepositorio;
 
     @Transactional
-    public void crearUsuario(String nombre, String apellido, String email, String password, String password2) throws MiException {
-        Usuario usuario = new Usuario();
-        validar(nombre, apellido, email, password, password2);
-        usuario.setNombre(nombre);
-        usuario.setApellido(apellido);
-        usuario.setEmail(email);
-        usuario.setPassword(new BCryptPasswordEncoder().encode(password));
-        usuario.setRol(Rol.USUARIO);
+    public void crearUsuarioProfesional(String nombre,String apellido,String email,String password,String password2,Especialidad especialidad,String descripcionEspecialidad,Integer reputacion,Integer valorConsulta,String matricula
+            ,String dni,String direccion,String telefono,ArrayList<ObraSocial> obrasSociales) throws MiException {
+        UsuarioProfesional usuarioProfesional = new UsuarioProfesional();
 
-        usuarioRepositorio.save(usuario);
+        validarProfesional( nombre, apellido, email, password, password2, especialidad, descripcionEspecialidad, reputacion, valorConsulta, matricula, dni, direccion, telefono, obrasSociales);
+        usuarioProfesional.setNombre(nombre);
+        usuarioProfesional.setApellido(apellido);
+        usuarioProfesional.setEmail(email);
+        usuarioProfesional.setPassword(new BCryptPasswordEncoder().encode(password));
+        usuarioProfesional.setRol(Rol.PROFESIONAL);
+        usuarioProfesional.setEspecialidad(especialidad);
+        usuarioProfesional.setDescripcionEspecialidad(descripcionEspecialidad);
+        usuarioProfesional.setReputacion(reputacion);
+        usuarioProfesional.setValorConsulta(valorConsulta);
+        usuarioProfesional.setMatricula(matricula);
+        usuarioProfesional.setDni(dni);
+        usuarioProfesional.setDireccion(direccion);
+        usuarioProfesional.setTelefono(telefono);
+        usuarioProfesional.setEstado(true);
+        usuarioProfesional.setObrasSociales(obrasSociales);
+
+        usuarioRepositorio.save(usuarioProfesional);
+    }    public void crearUsuarioPaciente(String nombre, String apellido, String email, String password, String password2,ObraSocial obraSocial, String dni, String direccion, String telefono) throws MiException {
+
+        UsuarioPaciente usuarioPaciente =new UsuarioPaciente();
+        validarPaciente(nombre, apellido, email, password, password2, obraSocial, dni, direccion, telefono);
+        usuarioPaciente.setNombre(nombre);
+        usuarioPaciente.setApellido(apellido);
+        usuarioPaciente.setEmail(email);
+        usuarioPaciente.setPassword(new BCryptPasswordEncoder().encode(password));
+        usuarioPaciente.setRol(Rol.PACIENTE);
+        usuarioPaciente.setDni(dni);
+        usuarioPaciente.setObraSocial(obraSocial);
+        usuarioPaciente.setDireccion(direccion);
+        usuarioPaciente.setTelefono(telefono);
+        usuarioPaciente.setEstado(true);
+        usuarioRepositorio.save(usuarioPaciente);
     }
+
 
     @Transactional
-    public void modificar(String nombre, String apellido, String email, String password, String password2) throws MiException {
-        validar(nombre, apellido, email, password, password2);
+    public void modificarProfesional(String nombre, String apellido, String email, String password, String password2, Especialidad especialidad, String descripcionEspecialidad, Integer reputacion, Integer valorConsulta, String matricula, String dni, String direccion, String telefono, Boolean estado, ArrayList<ObraSocial> obrasSociales) throws MiException {
+        validarProfesional(nombre, apellido, email, password, password2, especialidad, descripcionEspecialidad, reputacion, valorConsulta, matricula, dni, direccion, telefono, obrasSociales);
 
-        Optional<Usuario> respuesta = usuarioRepositorio.findById(email);
+        Optional<UsuarioProfesional> respuesta = usuarioRepositorio.buscarPorEmail(email);
 
         if (respuesta.isPresent()) {
+            UsuarioProfesional usuarioProfesional = respuesta.get();
 
-            Usuario usuario = respuesta.get();
+            usuarioProfesional.setNombre(nombre);
+            usuarioProfesional.setApellido(apellido);
+            usuarioProfesional.setEmail(email);
+            usuarioProfesional.setPassword(new BCryptPasswordEncoder().encode(password));
+            usuarioProfesional.setEspecialidad(especialidad);
+            usuarioProfesional.setDescripcionEspecialidad(descripcionEspecialidad);
+            usuarioProfesional.setReputacion(reputacion);
+            usuarioProfesional.setValorConsulta(valorConsulta);
+            usuarioProfesional.setMatricula(matricula);
+            usuarioProfesional.setDni(dni);
+            usuarioProfesional.setDireccion(direccion);
+            usuarioProfesional.setTelefono(telefono);
+            usuarioProfesional.setEstado(estado);
+            usuarioProfesional.setObrasSociales(obrasSociales);
 
-            usuario.setNombre(nombre);
-            usuario.setApellido(apellido);
-            usuario.setEmail(email);
-            usuario.setPassword(new BCryptPasswordEncoder().encode(password));
-
-            
-            usuarioRepositorio.save(usuario);
+            usuarioRepositorio.save(usuarioProfesional);
         }
-
     }
-    
+
     @Transactional(readOnly = true)
-    public List<Usuario> listarUsuarios() {
-        
-        List<Usuario> usuarios = new ArrayList();
-        
-        usuarios = usuarioRepositorio.findAll();
-        
-        return usuarios;
+    public List<Usuario> listarUsuariosPaciente() {
+
+        List<Usuario> usuariosPaciente = new ArrayList();
+        usuariosPaciente = usuarioRepositorio.findAll();
+
+        return usuariosPaciente;
 
     }
-    
+    @Transactional(readOnly = true)
+    public List<Usuario> listarUsuariosProfesional() {
+
+        List<Usuario> usuariosProfesional = new ArrayList();
+
+        usuariosProfesional = usuarioRepositorio.findAll();
+
+        return usuariosProfesional;
+
+    }
+
+
     public Usuario getOne(String email) {
         return usuarioRepositorio.getReferenceById(email);
     }
-    
+
     @Transactional
     public Usuario buscarPorId(String id) {
         return usuarioRepositorio.getReferenceById(id);
     }
 
-    public void validar(String nombre, String apellido, String email, String password, String password2) throws MiException {
+    public void validarProfesional(String nombre, String apellido, String email, String password, String password2, Especialidad especialidad, String descripcionEspecialidad, Integer reputacion, Integer valorConsulta, String matricula, String dni, String direccion, String telefono, ArrayList<ObraSocial> obrasSociales) throws MiException {
+        if (nombre == null || nombre.isEmpty()) {
+            throw new MiException("El nombre no puede ser nulo ni vacío");
+        }
+        if (apellido == null || apellido.isEmpty()) {
+            throw new MiException("El apellido no puede ser nulo ni vacío");
+        }
+        if (email == null || email.isEmpty()) {
+            throw new MiException("El email no puede ser nulo ni vacío");
+        }
+        if (password == null || password.isEmpty() || password.length() <= 5) {
+            throw new MiException("El password no puede ser nulo ni vacío, y debe contener más de 5 caracteres");
+        }
+        if (!password.equals(password2)) {
+            throw new MiException("Los passwords deben ser iguales");
+        }
+        if (especialidad == null) {
+            throw new MiException("La especialidad no puede ser nula");
+        }
+        if (descripcionEspecialidad == null || descripcionEspecialidad.isEmpty()) {
+            throw new MiException("La descripción de la especialidad no puede ser nula ni vacía");
+        }
+        if (reputacion == null || reputacion < 0) {
+            throw new MiException("La reputación debe ser un número entero positivo");
+        }
+        if (valorConsulta == null || valorConsulta < 0) {
+            throw new MiException("El valor de consulta debe ser un número entero positivo");
+        }
+        if (matricula == null || matricula.isEmpty()) {
+            throw new MiException("La matrícula no puede ser nula ni vacía");
+        }
+        if (dni == null || dni.isEmpty()) {
+            throw new MiException("El DNI no puede ser nulo ni vacío");
+        }
+        if (direccion == null || direccion.isEmpty()) {
+            throw new MiException("La dirección no puede ser nula ni vacía");
+        }
+        if (telefono == null || telefono.isEmpty()) {
+            throw new MiException("El teléfono no puede ser nulo ni vacío");
+        }
+        if (obrasSociales == null || obrasSociales.isEmpty()) {
+            throw new MiException("La lista de obras sociales no puede ser nula o vacía");
+        }
+    }
+    public void validarPaciente(String nombre, String apellido, String email, String password, String password2, ObraSocial obraSocial, String dni, String direccion, String telefono) throws MiException {
         if (nombre == null || nombre.isEmpty()) {
             throw new MiException("El nombre no puede ser nulo ni vacio");
         }
@@ -89,7 +182,18 @@ public class UsuarioServicio {
         if (!password.equals(password2)) {
             throw new MiException("Los passwords deben ser iguales");
         }
+        if (obraSocial == null) {
+            throw new MiException("La obra social no puede ser nula");
+        }
+        if (dni == null || dni.isEmpty()) {
+            throw new MiException("El DNI no puede ser nulo ni vacío");
+        }
+        if (direccion == null || direccion.isEmpty()) {
+            throw new MiException("La dirección no puede ser nula ni vacía");
+        }
+        if (telefono == null || telefono.isEmpty()) {
+            throw new MiException("El teléfono no puede ser nulo ni vacío");
+        }
 
     }
-
 }
