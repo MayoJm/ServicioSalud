6da852262dece7e4a4934505a2a5a4bb8962d4cb
juandiazmayo <juandiazmaio@gmail.com>
2024-02-23 16:10:24 -0300

    version final

--- src/main/java/com/appsalud/plataformaSalud/controladores/UsuarioProfesionalControlador.java
+++ src/main/java/com/appsalud/plataformaSalud/controladores/UsuarioProfesionalControlador.java
@@ -4,7 +4,9 @@ import com.appsalud.plataformaSalud.entidades.*;
 import com.appsalud.plataformaSalud.enumeraciones.Especialidad;
 import com.appsalud.plataformaSalud.enumeraciones.ObraSocial;
 import com.appsalud.plataformaSalud.excepciones.MiException;
+import com.appsalud.plataformaSalud.servicios.HistoriaClinicaServicio;
 import com.appsalud.plataformaSalud.servicios.TurnoServicio;
+import com.appsalud.plataformaSalud.servicios.UsuarioPacienteServicio;
 import com.appsalud.plataformaSalud.servicios.UsuarioProfesionalServicio;
 import com.appsalud.plataformaSalud.servicios.UsuarioServicio;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,9 +23,12 @@ import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.io.IOException;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -37,6 +42,10 @@ public class UsuarioProfesionalControlador {
     private UsuarioServicio usuarioServicio;
     @Autowired
     private TurnoServicio turnoServicio;
+    @Autowired
+    private HistoriaClinicaServicio historiaClinicaServicio;
+    @Autowired
+    private UsuarioPacienteServicio usuarioPacienteServicio;
 
     @PreAuthorize("hasRole('ROLE_PROFESIONAL')")
     @GetMapping("/dashboard-profesional")
@@ -65,19 +74,19 @@ public class UsuarioProfesionalControlador {
 
     @PostMapping("/registroProfesional")
     public String registroProfesional(@RequestParam String nombre,
-                                      @RequestParam String apellido,
-                                      @RequestParam String email,
-                                      @RequestParam String password,
-                                      @RequestParam String password2,
-                                      @RequestParam Especialidad especialidad,
-                                      @RequestParam String descripcionEspecialidad,
-                                      @RequestParam Integer valorConsulta,
-                                      @RequestParam String matricula,
-                                      @RequestParam String dni,
-                                      @RequestParam String direccion,
-                                      @RequestParam String telefono,
-                                      @RequestParam List<ObraSocial> obrasSociales,
-                                      Model model) {
+            @RequestParam String apellido,
+            @RequestParam String email,
+            @RequestParam String password,
+            @RequestParam String password2,
+            @RequestParam Especialidad especialidad,
+            @RequestParam String descripcionEspecialidad,
+            @RequestParam Integer valorConsulta,
+            @RequestParam String matricula,
+            @RequestParam String dni,
+            @RequestParam String direccion,
+            @RequestParam String telefono,
+            @RequestParam List<ObraSocial> obrasSociales,
+            Model model) {
         try {
             usuarioProfesionalServicio.crearUsuarioProfesional(nombre, apellido, email, password, password2,
                     especialidad,
@@ -134,17 +143,17 @@ public class UsuarioProfesionalControlador {
 
     @PostMapping("/profesionalForm")
     public String modificarProfesional(@RequestParam String nombre,
-                                       @RequestParam String apellido,
-                                       @RequestParam String passwordActual,
-                                       @RequestParam String nuevoPassword,
-                                       @RequestParam Especialidad especialidad,
-                                       @RequestParam String descripcionEspecialidad,
-                                       @RequestParam Integer valorConsulta,
+            @RequestParam String apellido,
+            @RequestParam String passwordActual,
+            @RequestParam String nuevoPassword,
+            @RequestParam Especialidad especialidad,
+            @RequestParam String descripcionEspecialidad,
+            @RequestParam Integer valorConsulta,
 
-                                       @RequestParam String dni,
-                                       @RequestParam String direccion,
-                                       @RequestParam String telefono,
-                                       Model model) {
+            @RequestParam String dni,
+            @RequestParam String direccion,
+            @RequestParam String telefono,
+            Model model) {
         try {
             Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
             String email = authentication.getName();
@@ -258,53 +267,153 @@ public class UsuarioProfesionalControlador {
         List<Turno> turnos = turnoServicio.listarTurnosPorProfesional(email);
         Optional<UsuarioProfesional> usuarioProfesionalOptional = usuarioProfesionalServicio
                 .buscarProfesionalPorEmail(email);
-        UsuarioProfesional profesional = usuarioProfesionalOptional.get();
+        UsuarioProfesional usuarioProfesional = usuarioProfesionalOptional.get();
         List<UsuarioPaciente> pacientes = usuarioProfesionalServicio.obtenerPacientes(email, turnos);
         model.addAttribute("pacientes", pacientes);
-        model.addAttribute("usuarioProfesional", profesional);
+        model.addAttribute("usuarioProfesional", usuarioProfesional);
 
         return "misPacientes.html";
     }
 
-    @GetMapping("/dashboard-profesional/turnos-disponibles")
-    public String mostrarFormularioTurnosDisponibles (Model model){
+    @PostMapping("/dashboard-profesional/mis-pacientes/seleccionar-paciente")
+    public String mostrarInfoPaciente(@RequestParam("pacienteId") String pacienteId,
+            RedirectAttributes redirectAttributes, Model model) {
+        System.out.println("Paciente id: " + pacienteId);
+        System.out.println(
+                "                                                                                                                                ");
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         String email = authentication.getName();
-
         Optional<UsuarioProfesional> usuarioProfesionalOptional = usuarioProfesionalServicio
                 .buscarProfesionalPorEmail(email);
-        UsuarioProfesional usuarioProfesional;
-        usuarioProfesional = usuarioProfesionalOptional.get();
-        model.addAttribute("usuarioProfesional", usuarioProfesional);
+        UsuarioProfesional profesional = usuarioProfesionalOptional.get();
+        UsuarioPaciente paciente = usuarioPacienteServicio.buscarPorId(pacienteId);
+        boolean tieneHistoriaClinica = true;
+        tieneHistoriaClinica = historiaClinicaServicio.tieneHistoriaClinica(paciente, profesional);
 
-        List<Turno> turnos= turnoServicio.listarTurnosPorProfesional(email);
-        model.addAttribute("turnos", turnos);
-        return "turnosDisponiblesProfesional.html";
+        if (tieneHistoriaClinica) {
+            redirectAttributes.addAttribute("pacienteId", pacienteId);
+            return "redirect:/profesional/dashboard-profesional/mis-pacientes/seleccionar-paciente/ver-historia-clinica";
+        } else {
+            redirectAttributes.addAttribute("pacienteId", pacienteId);
+            return "redirect:/profesional/dashboard-profesional/mis-pacientes/seleccionar-paciente/crear-historia-clinica";
+        }
     }
-    @GetMapping("/dashboard-profesional/turnos-disponibles/modificar-turno/{id}")
-    public String modificarTurno(@PathVariable String id, Model model) {
+
+    @GetMapping("/dashboard-profesional/mis-pacientes/seleccionar-paciente/ver-historia-clinica")
+    public String mostrarVistaVisualizacion(@RequestParam("pacienteId") String pacienteId, Model model) {
+        UsuarioPaciente paciente = usuarioPacienteServicio.buscarPorId(pacienteId);
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         String email = authentication.getName();
-
         Optional<UsuarioProfesional> usuarioProfesionalOptional = usuarioProfesionalServicio
                 .buscarProfesionalPorEmail(email);
         UsuarioProfesional usuarioProfesional = usuarioProfesionalOptional.get();
-        return "modificarTurno.html";
+        Optional<HistoriaClinica> historiaClinicaOptional = historiaClinicaServicio.obtenerHistoriaClinica(paciente,
+                usuarioProfesional);
+        if (historiaClinicaOptional.isPresent()) {
+            HistoriaClinica historiaClinica = historiaClinicaOptional.get();
+            model.addAttribute("paciente", paciente);
+            model.addAttribute("historiaClinica", historiaClinica);
+            model.addAttribute("usuarioProfesional", usuarioProfesional);
+            return "historia-clinica-paciente.html";
+
+        } else {
+            model.addAttribute("error", "No se encontró ninguna historia clínica para el paciente seleccionado.");
+            return "redirect:/dashboard-profesional/mis-pacientes";
+        }
+
     }
 
-    @GetMapping("/dashboard-profesional/turnos-disponibles/modificar-turno/horarios-disponibles")
-    @ResponseBody
-    public ResponseEntity<List<DisponibilidadHoraria>> obtenerHorariosDisponibles(
-            @RequestParam("profesionalId") String profesionalId,
-            @RequestParam("fecha") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate fechaSeleccionada)
-            throws MiException {
-
-        System.out.println("profesionalId: " + profesionalId);
-        UsuarioProfesional profesional = usuarioProfesionalServicio.buscarPorId(profesionalId);
-        System.out.println("profesional: " + profesional.getNombre());
-
-        List<DisponibilidadHoraria> horariosDisponibles = turnoServicio.obtenerHorariosDisponiblesParaDia(profesional,
-                fechaSeleccionada);
-        return ResponseEntity.ok().body(horariosDisponibles);
+    @GetMapping("/dashboard-profesional/mis-pacientes/seleccionar-paciente/crear-historia-clinica")
+    public String mostrarVistaCreacion(Model model, @RequestParam("pacienteId") String pacienteId) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String email = authentication.getName();
+        Optional<UsuarioProfesional> usuarioProfesionalOptional = usuarioProfesionalServicio
+                .buscarProfesionalPorEmail(email);
+        UsuarioProfesional usuarioProfesional = usuarioProfesionalOptional.get();
+        model.addAttribute("pacienteId", pacienteId);
+        model.addAttribute("usuarioProfesional", usuarioProfesional);
+        return "crearHistoriaClinica.html";
     }
+
+    @PostMapping("/dashboard-profesional/mis-pacientes/historia-clinica-crear")
+    public String crearHistoriaClinica(
+            @RequestParam("pacienteId") String pacienteId,
+            @RequestParam("edad") Integer edad,
+            @RequestParam("peso") double peso,
+            @RequestParam("sexo") String sexo,
+            @RequestParam("fechaConsulta") LocalDate fechaConsulta,
+            @RequestParam("notas") String notas,
+            Model model,
+            RedirectAttributes redirectAttributes) {
+        System.out.println("Paciente id: " + pacienteId);
+        System.out.println("Edad: " + edad);
+        System.out.println("Peso: " + peso);
+        System.out.println("Sexo: " + sexo);
+        System.out.println("Fecha consulta: " + fechaConsulta);
+        System.out.println("Notas: " + notas);
+
+        try {
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            String email = authentication.getName();
+            Optional<UsuarioProfesional> usuarioProfesionalOptional = usuarioProfesionalServicio
+                    .buscarProfesionalPorEmail(email);
+            UsuarioProfesional profesional = usuarioProfesionalOptional.get();
+            UsuarioPaciente paciente = usuarioPacienteServicio.buscarPorId(pacienteId);
+            String nombre = paciente.getNombre();
+            historiaClinicaServicio.crearHistoriaClinica(paciente, profesional, nombre, edad, sexo, peso, notas,
+                    fechaConsulta, true);
+            redirectAttributes.addFlashAttribute("exito", "Historia clinica creada con exito");
+            return "redirect:/profesional/dashboard-profesional/mis-pacientes";
+        } catch (MiException e) {
+            redirectAttributes.addAttribute("error", e.getMessage());
+            return "redirect:/profesional/dashboard-profesional/mis-pacientes";
+        }
+
+    }
+
+    @PostMapping("/dashboard-profesional/mis-pacientes/seleccionar-paciente/historia-clinica-editar")
+    public String editarHistoriaClinica(
+            @RequestParam("pacienteId") String pacienteId,
+            @RequestParam("nombre") String nombre,
+            @RequestParam("edad") Integer edad,
+            @RequestParam("sexo") String sexo,
+            @RequestParam("peso") double peso,
+            @RequestParam("nuevoDatoHistorico") String nuevoDatoHistorico,
+            @RequestParam("nuevaFechaConsulta") LocalDate nuevaFechaConsulta,
+            RedirectAttributes redirectAttributes, Model model) {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String email = authentication.getName();
+        Optional<UsuarioProfesional> usuarioProfesionalOptional = usuarioProfesionalServicio
+                .buscarProfesionalPorEmail(email);
+        UsuarioProfesional profesional = usuarioProfesionalOptional.get();
+
+        try {
+            UsuarioPaciente paciente = usuarioPacienteServicio.buscarPorId(pacienteId);
+            Optional<HistoriaClinica> historiaClinicaOptional = historiaClinicaServicio.obtenerHistoriaClinica(paciente,
+                    profesional);
+
+            if (paciente != null && profesional != null && historiaClinicaOptional.isPresent()) {
+                HistoriaClinica historiaClinica = historiaClinicaOptional.get();
+                List<LocalDate> fechasConsulta = historiaClinica.getFechaConsulta();
+                List<String> datosHistoricos = historiaClinica.getDatosHistoricos();
+                fechasConsulta.add(nuevaFechaConsulta);
+                datosHistoricos.add(nuevoDatoHistorico);
+                historiaClinicaServicio.modificarHistoriaClinica(historiaClinica, nombre, edad, sexo, peso,
+                        datosHistoricos,
+                        fechasConsulta);
+                redirectAttributes.addFlashAttribute("exito", "Historia clinica modificada con exito");
+                return "redirect:/profesional/dashboard-profesional/mis-pacientes";
+            } else {
+                redirectAttributes.addAttribute("error",
+                        "No se encontró ninguna historia clínica para el paciente seleccionado.");
+                return "redirect:/profesional/dashboard-profesional/mis-pacientes";
+            }
+
+        } catch (MiException e) {
+            redirectAttributes.addAttribute("error", e.getMessage());
+            return "redirect:/profesional/dashboard-profesional/mis-pacientes";
+        }
+    }
+
 }
--- src/main/java/com/appsalud/plataformaSalud/servicios/HistoriaClinicaServicio.java
+++ src/main/java/com/appsalud/plataformaSalud/servicios/HistoriaClinicaServicio.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -22,38 +23,45 @@ public class HistoriaClinicaServicio {
     @Transactional
     public void crearHistoriaClinica(UsuarioPaciente usuariopaciente, UsuarioProfesional usuarioProfesional,
             String nombre, Integer edad,
-            String sexo, Double peso, List<String> datosHistoricos, List<LocalDate> fechaConsulta, Boolean alta)
+            String sexo, Double peso, String notas, LocalDate fechaConsulta, Boolean alta)
             throws MiException {
-        validar(nombre, edad, sexo, peso, datosHistoricos, fechaConsulta);
+        List<String> datosHistoricos = new ArrayList<String>();
+        datosHistoricos.add(notas);
+        List<LocalDate> fechas = new ArrayList<LocalDate>();
+        fechas.add(fechaConsulta);
+        validar(nombre, edad, sexo, peso, datosHistoricos, fechas);
+        System.out.println("datos validados");
 
         HistoriaClinica historiaClinica = new HistoriaClinica();
+
+        System.out.println("historiaClinica creada");
         historiaClinica.setUsuarioPaciente(usuariopaciente);
         historiaClinica.setUsuarioProfesional(usuarioProfesional);
         historiaClinica.setEdad(edad);
         historiaClinica.setSexo(sexo);
         historiaClinica.setPeso(peso);
+        historiaClinica.setNombre(nombre);
         historiaClinica.setDatosHistoricos(datosHistoricos);
-        historiaClinica.setFechaConsulta(fechaConsulta);
-        historiaClinica.setAlta(true);
+        historiaClinica.setFechaConsulta(fechas);
+        historiaClinica.setAlta(alta);
 
         historiaClinicaRepositorio.save(historiaClinica);
     }
 
     @Transactional
-    public void modificarHistoriaClinica(String id, String nombre, Integer edad, String sexo, Double peso,
+    public void modificarHistoriaClinica(HistoriaClinica historiaClinica, String nombre, Integer edad, String sexo,
+            Double peso,
             List<String> datosHistoricos, List<LocalDate> fechaConsulta) throws MiException {
         validar(nombre, edad, sexo, peso, datosHistoricos, fechaConsulta);
 
-        Optional<HistoriaClinica> respuesta = historiaClinicaRepositorio.findById(id);
-        if (respuesta.isPresent()) {
-            HistoriaClinica historiaClinica = respuesta.get();
-            historiaClinica.setEdad(edad);
-            historiaClinica.setSexo(sexo);
-            historiaClinica.setPeso(peso);
-            historiaClinica.setDatosHistoricos(datosHistoricos);
-            historiaClinica.setFechaConsulta(fechaConsulta);
-            historiaClinicaRepositorio.save(historiaClinica);
-        }
+        historiaClinica.setNombre(nombre);
+        historiaClinica.setEdad(edad);
+        historiaClinica.setSexo(sexo);
+        historiaClinica.setPeso(peso);
+        historiaClinica.setDatosHistoricos(datosHistoricos);
+        historiaClinica.setFechaConsulta(fechaConsulta);
+        historiaClinicaRepositorio.save(historiaClinica);
+
     }
 
     @Transactional
@@ -67,7 +75,7 @@ public class HistoriaClinicaServicio {
     }
 
     public void validar(String nombre, Integer edad, String sexo, Double peso, List<String> datosHistoricos,
-            List<LocalDate> fechaConsulta) throws MiException {
+            List<LocalDate> fechas) throws MiException {
         if (nombre == null || nombre.isEmpty()) {
             throw new MiException("El nombre no puede ser vacio");
         }
@@ -83,9 +91,22 @@ public class HistoriaClinicaServicio {
         if (datosHistoricos == null || datosHistoricos.isEmpty()) {
             throw new MiException("Los datos historicos no pueden ser vacios");
         }
-        if (fechaConsulta == null || fechaConsulta.isEmpty()) {
+        if (fechas == null || fechas.isEmpty()) {
             throw new MiException("La fecha de consulta no puede ser vacia");
         }
+    }
+
+    public boolean tieneHistoriaClinica(UsuarioPaciente paciente, UsuarioProfesional profesional) {
+        // Buscar la historia clínica del paciente por su ID
+        Optional<HistoriaClinica> historiaClinica = historiaClinicaRepositorio.findByPacienteAndProfesional(paciente,
+                profesional);
+
+        // Devolver true si se encuentra la historia clínica, false en caso contrario
+        return historiaClinica.isPresent();
+    }
+
+    public Optional<HistoriaClinica> obtenerHistoriaClinica(UsuarioPaciente paciente, UsuarioProfesional profesional) {
+        return historiaClinicaRepositorio.findByPacienteAndProfesional(paciente, profesional);
     }
 
 }
